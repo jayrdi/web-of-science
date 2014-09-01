2af75b01422051454574835b4c2377b19458aa28
John Dawson <john@artgecko.co.uk>
2014-09-01 15:25:04 +0100

    update 01/09/14

--- soaptest.php
+++ soaptest.php
@@ -1,33 +1,36 @@
 <?php
 
 	// set variable to WSDL link to use elsewhere easily
-	$wsdl = "http://www.restfulwebservices.net/wcf/StockQuoteService.svc?wsdl";
+	$wsdl = "http://msrmaps.com/TerraService2.asmx?WSDL";
 
-	// create a SoapClient object
-	$client = new SoapClient($wsdl);
+    // for tracking errors
+    $trace = true;
+    $exceptions = true;
 
-	// store value as variable to be passed into the 'request' tag in the XML document
-	$stock = "NCR";
-    
-    // make an associative array and map 'request' to our previously established value (NCR)
-    // HAS TO MATCH TAGS OF ELEMENT IN SOAP REQUEST (in this case 'request' //
-    $parameters= array("request"=>$stock);
-    
-    // sending request to soap server, retrieves XML, sifts through and finds method 'GetStockQuote', passes "NCR" into <result> tag and gets data back from soap server, saving it as $value
-    $values = $client->GetStockQuote($parameters);
+    // create new Soap Client object with relevant params
+    $client = new SoapClient ($wsdl, array(
+                                     'trace' => $trace,
+                                     'exceptions' => $exceptions
+                                          )
+                             );
 
-    // use method 'GetStockQuoteResult' to return data as a stdClass Object with attributes
-    $xml = $values->GetStockQuoteResult;
-    
-    // print out XML data for request and response, print_r prints 'human-readable' information about an object, <pre> provides preformatting, i.e. new line (\n) after each bit of data
+    // create data array to send in SOAP request params
+    $xml_array['placeName'] = 'Pomona';
+    $xml_array['MaxItems'] = 3;
+    $xml_array['imagePresence'] = true;
+
+    // try to send the request to the web service
+    try {
+        $response = $client->GetPlaceList($xml_array);
+    } catch (Exception $e) {
+        echo "Error!";
+        echo $e -> getMessage();
+        echo 'Last response: '.$client->__getLastResponse();
+    }
+
+    // display results
     print "<pre>\n";
-    print_r($xml);
+    print_r($response);
     print "</pre>";
-
-    // store data from XML document that is referenced by 'Last'
-    $currentprice = $xml->Last;
-    
-    // print ONLY this bit of data from the object
-    print "<br />\n Last Value: $currentprice";
 
 ?>
--- wos.php
+++ wos.php
@@ -1,6 +1,23 @@
 <?php
 
-    // arogozin@nyu.edu
+    /*echo '<link rel="stylesheet" type="text/css" href="style.css"/>
+          <link href="http://fonts.googleapis.com/css?family=Raleway:700" rel="stylesheet" type="text/css">
+          <link href="http://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet" type="text/css">
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>'; */
+
+    // =================================================================== //
+    // == Author: John Dawson                                           == //
+    // == Date: 28/08/2014                                              == //
+    // == Description: Processing for a website to query Web of Science == //
+    // ==              Web Service using their API and return relevant  == //
+    // ==              data                                             == //
+    // =================================================================== //
+
+    
+    // =================================================================== //
+    // ================ SET UP SOAP CLIENTS & AUTHENTICATE =============== //
+    // =================================================================== //
+
 
     // set WSDL for authentication and create new SOAP client
     $auth_url  = "http://search.webofknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl";
@@ -20,11 +37,24 @@
     // call 'setCookie' method on '$search_client' storing SID (Session ID) as the response (value) given from the 'authenticate' method
     $search_client->__setCookie('SID',$auth_response->return);
 
+
+    // =================================================================== //
+    // ================= PRINT OUT REQUEST & RESPONSE ==================== //
+    // ====================== OF AUTHORISATION =========================== //
+    // =================================================================== // 
+
+
     // print details of XML request and response data for Authentication exchange
-    print "<pre>\n";
+    /* print "<pre>\n";
     print "<br />\n Request : ".htmlspecialchars($auth_client->__getLastRequest());
     print "<br />\n Response: ".htmlspecialchars($auth_client->__getLastResponse());
-    print "</pre>";
+    print "</pre>"; */
+
+
+    // =================================================================== //
+    // ============== PASS IN PARAMETERS FOR SOAP REQUEST ================ //
+    // =================================================================== //
+
 
     // pass in relevant parameters for search
     $search_array = array(
@@ -35,7 +65,7 @@
             'queryLanguage' => 'en'
         ),
         'retrieveParameters' => array(
-            'count' => '5',
+            'count' => '100',
             'sortField' => array(
                 array('name' => $_POST["sort"], 'sort' => 'D')
             ),
@@ -46,7 +76,7 @@
     /* POPULATE ARRAY VARIABLE WITH <uid> VALUES FROM $search_array INTO $retrieve_array */
 
     // pass in parameters for retrieveById
-    /*$retrieve_array = array(
+    /* $retrieve_array = array(
         'databaseId' => 'WOS',
         'uid' => 'WOS:A1993LC48100001 WOS:A1993LE28400012',
         'queryLanguage' => 'en',
@@ -55,6 +85,12 @@
             'firstRecord' => '1'
         )
     ); */
+
+
+    // =================================================================== //
+    // ======== PERFORM SEARCH USING PARAMETERS & SOAP CLIENT ============ //
+    // =================================================================== //
+
 
     // try to store as a variable the 'search' method on the '$search_array' called on the SOAP client with associated SID 
     try {
@@ -69,7 +105,11 @@
         echo $e->getMessage(); 
     }; */
 
-    echo "</br>SEARCH_RESPONSE: </br>";
+    // =================================================================== //
+    // ================ PRINT VALUES TO CHECK DATA ======================= //
+    // =================================================================== //
+
+    /* echo "</br>SEARCH_RESPONSE: </br>";
     print "<pre>\n";
     print_r($search_response);
     print "</pre>";
@@ -77,38 +117,114 @@
     echo "</br>SEARCH_CLIENT: </br>";
     print "<pre>\n";
     print_r($search_client);
-    print "</pre>";
+    print "</pre>"; */
 
-    // echo "</br>Search Response: </br>".$search_response->return->recordsFound;
-    // echo "</br>Search Client: </br>".$search_client->_cookies[0];
+    /* $string = htmlspecialchars($search_client->__getLastResponse());
 
-    /* echo "</br>RETRIEVE_RESPONSE: </br>";
+    // change all &lt; to <
+    // $string = str_replace('(&lt;)', '<', $string);
+
+    echo "</br></br>EXTRACTED STRING: </br></br>";
     print "<pre>\n";
-    print_r($retrieve_response);
+    print_r ($string);
     print "</pre>"; */
 
-    // print details of XML request and response data for Search exchange
-    /* print "<pre>\n";
-    print "<br />\n Request : ".$search_client->__getLastRequest();
-    print "<br />\n Response: ".$search_client->__getLastResponse();
+
+    // =================================================================== //
+    // ================= TURN SOAP RESPONSE STRING INTO ================== //
+    // ================== SIMPLE XML ELEMENT OBJECT TO =================== //
+    // ========== TRAVERSE AND EXTRACT INDIVIDUAL DATA ELEMENTS ========== //
+    // =================================================================== //
+
+
+    // turn Soap Client object into SimpleXMLElement
+    $xml = new SimpleXMLElement($search_response->return->records);
+
+    // register the namespaces
+    // $xml->registerXPathNamespace("ns1", "http://scientific.thomsonreuters.com/schema/wok5.4/public/FullRecord");
+    // $xml->registerXPathNamespace("ns2", "http://woksearch.v3.wokmws.thomsonreuters.com");
+    // $xml->registerXPathNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+
+    // initiate the xpath
+    // $xpath = "/soap:Envelope/soap:Body/ns2:searchResponse/return/records/ns1:records";
+
+    // $result = $xml->xpath($xpath);
+
+    /* echo "</br>SIMPLE XML ELEMENT OBJECT: </br>";
+    print "<pre>\n";
+    print_r($xml);
+    print "</pre>"; */
+    // print_r($result);
+
+    // encode XML data as JSON for use with Javascript
+    $json = json_encode($xml);
+    /* echo "</br>JSON: </br>";
+    print "<pre>\n";
+    print_r($json);
     print "</pre>"; */
 
-    /* echo "</br>var_dump on search client: </br>";
-    var_dump($search_client->__last_response); */
 
-    echo "</br>================================================================================================</br>";
+    // =================================================================== //
+    // ============ CREATE VARIABLES TO STORE REQUIRED DATA ============== //
+    // ================== FROM XML & DISPLAY IN TABLE ==================== //
+    // =================================================================== //
 
-    $string = $search_client->__getLastResponse();
 
-    echo "STRING: </br></br>".($string);
+    // journal name
+    /* $journal = "";
+    // publication name
+    $publication = "";
+    // various authors
+    $author1 = "";
+    $author2 = "";
+    $author3 = "";
+    $author4 = "";
+    $authorLast = "";
+    // address of author1
+    $address = "";
+    // number of citations for publication
+    $citations = 0;
+    // WoS unique identifier for publication
+    $uid = "";
+    // need to store all the authors for a given record as number of authors varies
+    $personArray = array();
 
-    $xml = simplexml_load_string($string);
-    $xml->registerXPathNamespace('soap', 'http://schemas.xmlsoap.org/soap/envelope/');
-    $xml->registerXPathNamespace('ns2', 'http://woksearch.v3.wokmws.thomsonreuters.com');
-    $xml->registerXPathNamespace('xmlns', 'http://scientific.thomsonreuters.com/schema/wok5.4/public/FullRecord');
+    // TESTS
+    foreach($xml->REC as $record) {
+        print "<pre>\n";
+        echo $record->static_data->summary->titles->title[0]."</br>";
+        echo $record->static_data->summary->titles->title[5]."</br>";
+        echo $record->static_data->summary->names->name[0]->full_name."</br>";
+        echo $record->static_data->fullrecord_metadata->addresses->address_name->address_spec->full_address."</br>";
+        echo $record->dynamic_data->citation_related->tc_list->silo_tc->attributes()."</br>";
+        echo $record->UID."</br>";
+        print "</pre>";
+    } */
 
-    foreach ($xml->xpath('//records/REC/UID/text()') as $item) {
-        print_r($item);
-    };
+    // print table with suitable headers
+    echo '<table id="table" <tr>
+                <th>Journal Name</th>
+                <th>Publication Name</th>
+                <th>Author</th>
+                <th>Address</th>
+                <th>Number of Citations</th>
+                <th>Unique Identifier</th>
+            </tr>>';
+
+    // print data in table
+    foreach($xml->REC as $record) {
+        echo '<tr>';
+        echo '<td>'.$record->static_data->summary->titles->title[0]."</td>";
+        echo '<td>'.$record->static_data->summary->titles->title[5]."</td>";
+        echo '<td>'.$record->static_data->summary->names->name[0]->full_name."</td>";
+        echo '<td>'.$record->static_data->fullrecord_metadata->addresses->address_name->address_spec->full_address."</td>";
+        echo '<td>'.$record->dynamic_data->citation_related->tc_list->silo_tc->attributes()."</td>";
+        echo '<td>'.$record->UID."</td>";
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    // file_put_contents("wosData.json", $json);
+
 
 ?>
